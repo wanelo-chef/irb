2bb30e099d8f8e305f2eda776f34b786ae3874dc
Eric Saxby & Richard Millan <eric+richard@wanelo.com>
2015-06-18 11:10:12 -0700

    Fix syntax error

--- providers/config.rb
+++ providers/config.rb
@@ -2,7 +2,7 @@ action :configure do
   template "#{new_resource.home_directory}/.irbrc" do
     source 'irbrc.erb'
     cookbook 'irc'
-    owner :user
+    owner new_resource.user
     mode 0755
   end
 end
